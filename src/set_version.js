2fa61c629a20cc36d313550a30878708fa6d3073
Joe Fleming <joe.fleming@gmail.com>
2017-07-20 10:56:13 -0700

    better version setting
    don't override the version, always set the kibana version

--- src/set_version.js
+++ src/set_version.js
@@ -4,11 +4,16 @@ module.exports = (targetPath, kibanaPath) => {
   const kibanaPkg = readJson(`${kibanaPath}/package.json`);
   const pluginPkg = readJson(`${targetPath}/package.json`);
 
-  if (pluginPkg.kibana && pluginPkg.kibana.version) {
-    pluginPkg.kibana.version = kibanaPkg.version;
-  } else {
-    pluginPkg.version = kibanaPkg.version;
+  // add the kibana property if it's missing
+  if (!pluginPkg.kibana) {
+    pluginPkg.kibana = {};
   }
+
+  // set the kibana.version
+  pluginPkg.kibana.version = kibanaPkg.version;
+
+  // clean up incorrect use of kibana.version
+  delete pluginPkg['kibana.version'];
 
   writeJson(`${targetPath}/package.json`, pluginPkg);
 
