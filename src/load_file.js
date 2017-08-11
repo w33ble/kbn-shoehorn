00746c51cfafc04fdb89ab8e5b826a8e2a197e6d
Joe Fleming <joe.fleming@gmail.com>
2017-08-10 17:03:39 -0700

    feat: traverse into zip paths
    when looking for plugin, if it's not found in the root, walk in as long as there is only one path in the target
    
    this is a fix for some plugins that ship as a zip file and expect you to
    unpack from kibana's root path

--- src/load_file.js
+++ src/load_file.js
@@ -1,26 +1,40 @@
-const { statSync } = require('fs');
+const { statSync, readdirSync } = require('fs');
 const { parse, join } = require('path');
 const ncp = require('ncp').ncp;
 const tempDir = require('temp-dir');
 const extract = require('extract-zip');
 const { readJson } = require('./json_file');
 
+// paths to ignore when attempting to traverse into plugin
+const ignorePaths = [
+  '.DS_Store',
+];
+
+function readPackageName(dirPath) {
+  try {
+    const pluginPkgFile = join(dirPath, 'package.json');
+    return readJson(pluginPkgFile).name;
+  } catch (e) {
+    return null;
+  }
+}
+
 function copyPath(filePath, targetPath) {
   return new Promise((resolve, reject) => {
-    try {
-      // make sure a package.json file exists
-      const pluginPkgFile = join(filePath, 'package.json');
-      const { name } = readJson(pluginPkgFile);
-      const pluginTarget = join(targetPath, name);
+    const pluginName = readPackageName(filePath);
 
-      ncp(filePath, pluginTarget, (err) => {
-        if (err) reject(err);
-        else resolve(pluginTarget);
-      });
-    } catch (e) {
-      if (e.code === 'ENOENT') reject(new Error('Plugin package.json not found'));
-      else reject(e);
+    // make sure a package.json file exists
+    if (!pluginName) {
+      reject(new Error('Failed to read name from plugin'));
+      return;
     }
+
+    const pluginTarget = join(targetPath, pluginName);
+
+    ncp(filePath, pluginTarget, (err) => {
+      if (err) reject(err);
+      else resolve(pluginTarget);
+    });
   });
 }
 
@@ -35,8 +49,30 @@ function unpackZip(filePath, targetPath) {
     }
 
     extract(filePath, { dir: unpackTarget }, (err) => {
-      if (err) reject(err);
-      else resolve(copyPath(unpackTarget, targetPath));
+      if (err) {
+        reject(err);
+        return;
+      }
+
+      // look for name in package.json, traverse if only one path is found
+      function getPluginPath(dirPath) {
+        const pluginName = readPackageName(dirPath);
+
+        if (!pluginName) {
+          const paths = readdirSync(dirPath).filter(path => ignorePaths.indexOf(path) === -1);
+          if (paths.length !== 1) throw new Error(`Plugin not found: ${filePath}`);
+
+          const deepDirPath = join(dirPath, paths[0]);
+          const stat = statSync(deepDirPath);
+          if (!stat.isDirectory()) throw new Error(`Plugin not found: ${filePath}`);
+
+          return getPluginPath(deepDirPath);
+        }
+
+        return dirPath;
+      }
+
+      resolve(copyPath(getPluginPath(unpackTarget), targetPath));
     });
   });
 }
